fdb0c15114149e32184e03cf194161fd829b9ab8
Iron.Snowflake <jadrubio@gmail.com>
2021-05-20 11:18:43 -0400

    hooked up service to make API call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  wikiResults = [];
+  
+  constructor(private wikipedia: WikipediaService) { }
   onTerm(term: string) {
-    console.log(`${term} in app`)
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.wikiResults = response.query.search;
+    });
   }
 }

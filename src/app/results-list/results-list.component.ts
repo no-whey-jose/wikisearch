7e278a3813150c01fac1c20c734c079510136415
Iron.Snowflake <jadrubio@gmail.com>
2021-05-20 14:20:28 -0400

    displaying in unstyled table the search results

--- src/app/results-list/results-list.component.ts
+++ src/app/results-list/results-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-results-list',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results-list.component.css']
 })
 export class ResultsListComponent implements OnInit {
+  @Input() wikiResults: any = [];
 
   constructor() { }
 
